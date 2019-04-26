a7afbb24ca9ad2e54f55e5baf5833adbc249df59
pedroamaral91 <pedro.vinicius01@gmail.com>
2019-04-26 14:15:11 -0300

    created auth route and auth controller

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const routes = express.Router();
 const UserController = require("./app/controllers/UserController");
+const SessionController = require("./app/controllers/SessionController");
 const multerConfig = require("./config/multer");
 const upload = require("multer")(multerConfig);
 
+routes.get("/signin", SessionController.create);
+routes.post("/signin", SessionController.store);
 routes.get("/signup", UserController.create);
 routes.post("/signup", upload.single("avatar"), UserController.store);
+routes.get("/app/dashboard", (req, res) => {
+  return res.render("dashboard");
+});
 
 module.exports = routes;
