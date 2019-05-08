de2d933d3431ba0670c163a74a44798f179e0ded
pedroamaral91 <pedro.vinicius91@gmail.com>
2019-05-08 09:35:08 -0300

    created logout structure and flash messages to show success and error in output.

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,26 @@
-const express = require("express");
-const routes = express.Router();
-const UserController = require("./app/controllers/UserController");
-const SessionController = require("./app/controllers/SessionController");
-const multerConfig = require("./config/multer");
-const upload = require("multer")(multerConfig);
+const express = require('express')
+const routes = express.Router()
+const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
 
-routes.get("/signin", SessionController.create);
-routes.post("/signin", SessionController.store);
-routes.get("/signup", UserController.create);
-routes.post("/signup", upload.single("avatar"), UserController.store);
-routes.get("/app/dashboard", (req, res) => {
-  return res.render("dashboard");
-});
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
+  return next()
+})
 
-module.exports = routes;
+routes.get('/', guestMiddleware, SessionController.create)
+routes.post('/signin', guestMiddleware, SessionController.store)
+routes.get('/signup', UserController.create)
+routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.use('/app', authMiddleware)
+routes.get('/app/logout', SessionController.destroy)
+
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
+
+module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,30 +1,45 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
-const path = require("path");
+const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require('path')
+const session = require('express-session')
+const LokiStore = require('connect-loki')(session)
+const flash = require('connect-flash')
 
 class App {
-  constructor() {
-    this.express = express();
-    this.isDev = process.env.NODE_ENV !== "production";
-    this.middlewares();
-    this.views();
-    this.routes();
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== 'production'
+    this.middlewares()
+    this.views()
+    this.routes()
   }
-  middlewares() {
-    this.express.use(express.urlencoded({ extended: false }));
+  middlewares () {
+    this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(flash())
+    this.express.use(
+      session({
+        name: 'root',
+        secret: 'MyAppSecret',
+        resave: false,
+        store: new LokiStore({
+          path: path.resolve(__dirname, '..', 'tmp', 'sessions.db')
+        }),
+        saveUninitialized: true
+      })
+    )
   }
-  views() {
-    nunjucks.configure(path.resolve(__dirname, "app", "views"), {
+  views () {
+    nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
       watch: this.isDev,
       express: this.express,
       autoescape: true
-    });
-    this.express.use(express.static(path.resolve(__dirname, "public")));
-    this.express.set("view engine", "njk");
+    })
+    this.express.use(express.static(path.resolve(__dirname, 'public')))
+    this.express.set('view engine', 'njk')
   }
-  routes() {
-    this.express.use(require("./routes"));
+  routes () {
+    this.express.use(require('./routes'))
   }
 }
 
-module.exports = new App().express;
+module.exports = new App().express
